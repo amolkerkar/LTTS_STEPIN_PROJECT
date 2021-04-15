02a10d4d0562e9a6881ddd60010fd17197c98df3
amol.kerkar <amolkerkar2@gmail.com>
2021-04-16 02:49:01 +0530

    trihggered

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main()
 {
-    printf("Welcome to Gthub Actions.\n******* Its Working**********\n");
+    printf("Welcome to Gthub Actions.\n********* Its Working**********\n");
     return 0;
 }
