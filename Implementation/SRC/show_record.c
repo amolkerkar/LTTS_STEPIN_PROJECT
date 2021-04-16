ec79bf072f9681fb56cec259f9f4c789d9220be8
264877 <81116875+amolkerkar@users.noreply.github.com>
2021-04-16 15:50:14 +0530

    Update show_record.c

--- Implementation/SRC/show_record.c
+++ Implementation/SRC/show_record.c
@@ -19,7 +19,7 @@ void show_record()
 	float scr;
 	FILE *f;
 	f=fopen("score.txt","r");
-	fscanf(f,"%s%f",&name,&scr);
+	fscanf(f,"%19s%f",name,&scr);
 	printf("\n\n\t\t*************************************************************");
 	printf("\n\n\t\t %s has secured the Highest Score %0.2f",name,scr);
 	printf("\n\n\t\t*************************************************************");
